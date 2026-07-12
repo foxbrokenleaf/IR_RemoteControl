660dedd03c8b7446e690d27424f1fa5084ed12e7
foxbrokenleaf <2353919304@qq.com>
2026-07-12 22:52:38 +0800

    OLED正常工作 按键正常工作

--- main.c
+++ main.c
@@ -3,13 +3,26 @@
 #include "oled_0_69inch.h"
 
 
-sbit P11 = P1 ^ 1;
+sbit IR = P1 ^ 1;       //P11F
+/*
+ * IR
+*/
+sbit Key_1 = P1 ^ 7;    //P17F
+sbit Key_2 = P2 ^ 1;    //P21F
+sbit Key_3 = P0 ^ 7;    //P07F
+sbit Key_4 = P0 ^ 6;    //P06F
+sbit Key_5 = P0 ^ 5;    //P05F
+/*************************
+ *       * Key_2 * Key_4 *
+ * Key_1 * Key_3 * Key_5 *
+ * ***********************
+*/
 
 
 /*
  * Define private funcation
 */
-
+void SystemInit(void);  //初始化所需要的外设
 
 /*
  *	Private var
@@ -23,22 +36,29 @@ static void delay(int i)
 }
 
 void main(){
-		
-    int i;
+    uint8_t KeyCode = 0;
 	
-    SCCON  = 0x00;//HRC
-    HRCON |= 0x80;
-	
-    P11F = 0x02;
-
-    P11 = 1;
-
+    SystemInit();
 	OLED_Init();
-    OLED_ShowString(0, 0, "0123456789", OLED_6X8);
-    OLED_Update();
+
 
     while(1){
+        
+        KeyCode = (uint8_t)(Key_1) | ((uint8_t)(Key_2) << 1) | ((uint8_t)(Key_3) << 2) | ((uint8_t)(Key_4) << 3) | ((uint8_t)(Key_5) << 4);
 
+        OLED_ShowNum(0, 0, KeyCode, 3, OLED_6X8);
+
+        OLED_Update();
     }
 }
 
+void SystemInit(void){
+    SCCON  = 0x00;//HRC
+    HRCON |= 0x80;
+    P11F = 0x02;
+    P17F = 0x01;
+    P21F = 0x01;
+    P07F = 0x01;
+    P06F = 0x01;
+    P05F = 0x01;
+}
